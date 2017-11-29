429a48143110ac40c8326e90f12ff3c83c7514f1
Miklos Kis <31532557+m-kis@users.noreply.github.com>
2017-11-29 10:40:47 +0100

    First weekly assignments part 1

--- src/com/mkis/assignments/gradientdescentandnormalequation/GradientDescentLinearRegressionOneVariable.java
+++ src/com/mkis/assignments/gradientdescentandnormalequation/GradientDescentLinearRegressionOneVariable.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.DoubleStream;
 
-//First weekly assignments
+//First weekly assignments part 1
 
 public class GradientDescentLinearRegressionOneVariable {
 
