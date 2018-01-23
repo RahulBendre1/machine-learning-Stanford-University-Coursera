b31c7dd32fc581c845c3f488a25aa7131ac1cc88
Miklos Kis <31532557+m-kis@users.noreply.github.com>
2018-01-23 11:29:58 +0100

    6th assignment part 1 - linear SVM

---
+++ src/com/mkis/assignments/supportvectormachines/SVM.java
@@ -0,0 +1,311 @@
+package com.mkis.assignments.supportvectormachines;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.*;
+import java.util.List;
+
+/*In the first half of this exercise, you will be using support vector machines
+(SVMs) with various example 2D datasets.*/
+
+public class SVM extends ApplicationFrame {
+
+    private static String file = "D:\\Projects-repos\\MachineLearning\\src\\com\\mkis\\assignments\\supportvectormachines\\data1.txt";
+    private static double m; // number of training examples
+    private static int n; // number of columns in dataset
+    private static double theta[]; // parameters/weights array
+    private static List<Instance> dataSet = new ArrayList<>(); //list containing 1 row of training example
+    private static List<Double> meansOfFeatures = new ArrayList<>(); //list containing the means of each feature
+    private static List<Double> maxMinusMinOfFeatures = new ArrayList<>(); //list containing max-min of each feature
+    private static double precision;
+    private static double recall;
+    private static double F1_score;
+    private static NumberFormat nf = new DecimalFormat("##.##");
+
+    //For plotting
+    private SVM(final String title) {
+
+        super(title);
+
+        // Create a single plot containing both the scatter and line
+        XYPlot plot = new XYPlot();
+
+        //SETUP SCATTER graph for admitted students
+        // Create the scatter data, renderer, and axis
+        XYSeries seriesEx1 = new XYSeries("1");
+        for (int i = 0; i < dataSet.size(); i++) {
+            if (dataSet.get(i).yValue == 1)
+                seriesEx1.add(dataSet.get(i).xVariables[0] * maxMinusMinOfFeatures.get(0) + meansOfFeatures.get(0),
+                        dataSet.get(i).xVariables[1] * maxMinusMinOfFeatures.get(1) + meansOfFeatures.get(1)); //feature scaling
+        }
+        XYDataset dataSetVisEx1 = getData(seriesEx1);
+        XYItemRenderer rendererDataEx1 = new XYLineAndShapeRenderer(false, true);
+        rendererDataEx1.setBasePaint(Color.GREEN);
+        ValueAxis xAxisDataEx1 = new NumberAxis("Exam 1 score");
+        ValueAxis yAxisDataEx1 = new NumberAxis("Exam 2 score");
+        xAxisDataEx1.setLowerBound(0);
+        xAxisDataEx1.setUpperBound(5);
+        yAxisDataEx1.setLowerBound(1.2);
+        yAxisDataEx1.setUpperBound(4.8);
+
+        // Set the scatter data, renderer, and axis into plot
+        plot.setDataset(0, dataSetVisEx1);
+        plot.setRenderer(0, rendererDataEx1);
+        plot.setDomainAxis(0, xAxisDataEx1);
+        plot.setRangeAxis(0, yAxisDataEx1);
+
+        //SETUP SCATTER graph for students not admitted
+        // Create the scatter data, renderer, and axis
+        XYSeries seriesEx2 = new XYSeries("2");
+        for (int i = 0; i < dataSet.size(); i++) {
+            if (dataSet.get(i).yValue == -1)
+                seriesEx2.add(dataSet.get(i).xVariables[0] * maxMinusMinOfFeatures.get(0) + meansOfFeatures.get(0),
+                        dataSet.get(i).xVariables[1] * maxMinusMinOfFeatures.get(1) + meansOfFeatures.get(1)); //feature scaling
+        }
+        XYDataset dataSetVisEx2 = getData(seriesEx2);
+        XYItemRenderer rendererDataEx2 = new XYLineAndShapeRenderer(false, true);
+
+        // Set the scatter data, renderer, and axis into plot
+        plot.setDataset(1, dataSetVisEx2);
+        plot.setRenderer(1, rendererDataEx2);
+
+        //SETUP LINE graph
+        // Create the line data, renderer, and axis
+        XYSeries lineSeries = new XYSeries("Hyperplane");
+        double x = 0.0;
+        while (x <= 5) {
+            lineSeries.add(x,
+                    ((theta[0] * ((x - meansOfFeatures.get(0)) / maxMinusMinOfFeatures.get(0))) / (-1 * theta[1])) * maxMinusMinOfFeatures.get(1) + meansOfFeatures.get(1)); //feature scaling
+            x += 0.05;
+        }
+        XYDataset lineDataSet = getLineData(lineSeries);
+        XYItemRenderer rendererLine = new XYLineAndShapeRenderer(true, false);   // Lines only
+
+        // Set the line data, renderer, and axis into plot
+        plot.setDataset(2, lineDataSet);
+        plot.setRenderer(2, rendererLine);
+
+        // Create the chart with the plot and a legend
+        JFreeChart chart = new JFreeChart("Data set - SVM", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 800));
+        setContentPane(chartPanel);
+    }
+
+    public static void main(String[] args) {
+
+        getNumberOfFeaturesAndTrainingExamples();
+        loadData();
+        init();
+        System.out.println("Cost Function at theta: " + Arrays.toString(theta) + " : " + createLossFunction(dataSet));
+        doGradientDescent(dataSet);
+        System.out.println("Cost Function at theta: " + Arrays.toString(theta) + " : " + createLossFunction(dataSet));
+        System.out.println("Accuracy of the model: " + calcAccuracyOfModel(dataSet) + " %");
+        System.out.println("Precision of the model: " + nf.format(precision));
+        System.out.println("Recall of the model: " + nf.format(recall));
+        System.out.println("F1-score: " + nf.format(F1_score) + "\n");
+
+        SVM visualizationOfData = new SVM("Visualization of data");
+        visualizationOfData.pack();
+        RefineryUtilities.centerFrameOnScreen(visualizationOfData);
+        visualizationOfData.setResizable(false);
+        visualizationOfData.setVisible(true);
+    }
+
+    //Get XYDataset data data for visualization
+    private XYDataset getData(XYSeries series) {
+        XYSeriesCollection xySeriesCollectionData = new XYSeriesCollection();
+        xySeriesCollectionData.addSeries(series);
+        return xySeriesCollectionData;
+    }
+
+    //Get XYDataset line data for visualization
+    private XYDataset getLineData(XYSeries lineSeries) {
+        XYSeriesCollection xySeriesCollectionLine = new XYSeriesCollection();
+        xySeriesCollectionLine.addSeries(lineSeries);
+        return xySeriesCollectionLine;
+    }
+
+    public static class Instance {
+        double yValue;
+        double[] xVariables;
+
+        Instance(double yValue, double[] xVariables) {
+            this.yValue = yValue;
+            this.xVariables = xVariables;
+        }
+    }
+
+    private static void getNumberOfFeaturesAndTrainingExamples() {
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            String[] columns;
+            List<List<Double>> tempList = new ArrayList<>();
+            m = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                columns = line.split(",");
+                n = columns.length;
+                if (m == 0) {
+                    for (int i = 0; i < n - 1; i++) {
+                        List<Double> tempInnerList = new ArrayList<>();  //create inner lists, number depends on the number of features
+                        tempList.add(i, tempInnerList);
+                    }
+                }
+                for (int i = 0; i < n - 1; i++) {
+                    tempList.get(i).add(Double.parseDouble(columns[i]));
+                }
+                m++;
+            }
+            //for mean normalization get means and (max-min)s of all the features
+            for (int i = 0; i < n - 1; i++) {
+                meansOfFeatures.add(i, tempList.get(i).stream().mapToDouble(val -> val).average().getAsDouble());
+                maxMinusMinOfFeatures.add(i, Collections.max(tempList.get(i)) - Collections.min(tempList.get(i)));
+            }
+            bufferedReader.close();
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Load the data from the txt file into an array
+    private static void loadData() {
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            String[] columns;
+            while ((line = bufferedReader.readLine()) != null) {
+                columns = line.split(",");
+                double y;
+                // y  in {-1,1}
+                if (Double.parseDouble(columns[n - 1]) == 0) {
+                    y = -1d;
+                } else {
+                    y = 1;
+                }
+                double xArray[] = new double[n - 1];
+                for (int i = 0; i < n - 1; i++) {
+                    xArray[i] = (Double.parseDouble(columns[i]) - meansOfFeatures.get(i)) / maxMinusMinOfFeatures.get(i); // feature scaling applied, to get variables between 0 and 1
+                }
+                //System.out.println(xArray[0] + " | " + xArray[1] + " | " + y );
+                Instance instance = new Instance(y, xArray);
+                dataSet.add(instance);
+            }
+            bufferedReader.close();
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Initialize theta with zeros
+    private static void init() {
+        theta = new double[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            theta[i] = 0.0;
+        }
+    }
+
+    //Hinge loss
+    private static double createLossFunction(List<Instance> instances) {
+        double cost = 0.0;
+        for (Instance instance : instances) {
+            double[] x = instance.xVariables;
+            double y = instance.yValue;
+            if (y * createHypothesis(x) >= 1) {
+                cost += 0;
+            } else {
+                cost += 1 - y * createHypothesis(x);
+            }
+        }
+        //System.out.println("Cost function value with theta " + Arrays.toString(theta) + ": " + J);
+        return cost;
+    }
+
+    //Sigmoid function
+    private static double sigmoid(double z) {
+        return 1.0 / (1.0 + Math.exp(-z));
+    }
+
+    //Creating our hypothesis (h(x))
+    private static double createHypothesis(double[] x) {
+        double hypothesis = 0.0;
+        for (int i = 0; i < n - 1; i++) {
+            hypothesis += theta[i] * x[i];
+        }
+        return sigmoid(hypothesis);
+    }
+
+    //Do gradient descent
+    private static void doGradientDescent(List<Instance> instances) {
+        double alpha = 0.5; //learning rate
+        double C = 1; //reg. term
+        for (int it = 1; it < 100; it++) {
+            double[] temp = new double[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                temp[i] = 0.0;
+            }
+            for (Instance instance : instances) {
+                double[] x = instance.xVariables;
+                double hypothesis = createHypothesis(x);
+                double y = instance.yValue;
+                if (y * hypothesis < 1) {
+                    for (int i = 0; i < n - 1; i++) {
+                        temp[i] += x[i] * y;
+                    }
+                }
+            }
+            for (int i = 0; i < n - 1; i++) {
+                theta[i] = (1 - alpha) * theta[i] + C * temp[i];
+            }
+            //System.out.println(createLossFunction(dataSet));
+        }
+    }
+
+    //Predict whether the label is -1 or 1 using
+    private static double predict(double[] scores) {
+        if (createHypothesis(scores) >= 0.5) return 1;
+        return -1;
+    }
+
+    //Calculate accuracy, precision, recall and F1-score
+    private static double calcAccuracyOfModel(List<Instance> instances) {
+        int counter = 0;
+        double truePositives = 0;
+        double falsePositives = 0;
+        double falseNegatives = 0;
+        for (Instance instance : instances) {
+            double[] x = instance.xVariables;
+            double y = instance.yValue;
+            if (predict(x) == 1 && y == 1) truePositives++;
+            if (predict(x) == -1 && y == 1) falseNegatives++;
+            if (predict(x) == 1 && y == -1) falsePositives++;
+            if (predict(x) != y) counter++;
+        }
+        precision = truePositives / (truePositives + falsePositives);
+        recall = truePositives / (truePositives + falseNegatives);
+        F1_score = 2 * precision * recall / (precision + recall);
+        return (1 - counter / m) * 100;
+    }
+
+}
